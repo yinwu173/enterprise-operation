c3363ffff040889acccebc3f702c5cfe681f00e2
yinwu173 <yin.wu173@gmail.com>
2024-11-04 21:20:52 -0800

    edited User.js: added User Model, made sure to validate email address format, retrieve length of the user's 'friend's' array, then exported the file. Edited Readme to include resource I used to help me match a valid email address

---
+++ README.md
@@ -0,0 +1,3 @@
+### Resources
+
+* For the lines of code from 17-21 of User.js, I received help utilizing the [Mongoose - Email Address Validation with Regex](https://blog.bounceless.io/mastering-email-validation-in-mongoose-syntax-uniqueness-and-beyond/) resource. The `validate` property will check to determine whether the email address inputted matches the regex for a valid email format.
---
+++ models/User.js
@@ -0,0 +1,55 @@
+const { Schema, model } = require('mongoose');
+
+// schema to create a User model
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            validate: {
+                validator: function(value) {
+                    // Email address validation with regex
+                    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+                },
+                message: 'Please enter a valid email address',
+            }
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            },
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+// Returns the length of the user's `friends` array field on query
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+// Create User model from userSchema
+const User = model('User', userSchema);
+
+
+module.exports = User;
