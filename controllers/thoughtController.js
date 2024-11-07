fd90a5692e0b2c3e9b0f90d7b56db507d436008f
yinwu173 <yin.wu173@gmail.com>
2024-11-06 18:39:17 -0800

    fixed syntax error: if no thought found with id, then return error.

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -17,7 +17,7 @@ const thoughtController = {
             const thought = await Thought.findOne({ _id: req.params.thoughtId })
                 .select('-__v');
 
-            if (thought) {
+            if (!thought) {
                 return res.status(404).json({ message: 'No thought with that ID' })
             }
 
